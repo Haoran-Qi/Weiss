a1471070636a45cd8bceb555b365a1c27fba0d70
DESKTOP-0191A3N\Devin <DevinQi@outlook.com>
2023-08-11 10:28:22 +0800

    init

---
+++ src/routes/5mm_SPC_Apollo/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Apollo from '../../5mmSPC/Apollo.jpg';
+    import Apollo2 from '../../5mmSPC/Apollo-2.jpg';
+    import Apollo3 from '../../5mmSPC/Apollo-3.jpg'
+</script>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column; /* Stack children vertically */
+    align-items: center;    /* Center children horizontally */
+    max-height: 100vh;      /* Maximum height is the viewport height */
+    overflow-y: auto;       /* Allow vertical scrolling if content overflows */
+    width: 100%;            /* Take full width */
+}
+
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.image-container {
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    width: 100%;            /* Take full width */
+}
+
+/* Style for each image */
+.image-container img {
+    max-width: 100%;       /* Images will not exceed the container's width */
+    height: auto;          /* Adjust the height automatically based on the image's aspect ratio */
+    object-fit: cover;     /* Resize the content of the image to fit its container */
+    margin-bottom: 10px;   /* Margin at the bottom of each image for separation */
+}
+</style>
+  
+  <main>
+    <h1>Apollo 5mm SPC</h1>
+  
+    <div class="image-container">
+      <img src={Apollo} alt="">
+      <img src={Apollo2} alt="">
+      <img src={Apollo3} alt="">
+    </div>
+  </main>
