c218363bc97efe1211e7c90a3d9f6e029eed13f0
DESKTOP-0191A3N\Devin <DevinQi@outlook.com>
2023-08-17 11:20:38 +0800

    fix for my son

---
+++ src/routes/365f456e67696e65657265645f426c61636b506570706572/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Apollo from '../../6_Engineered/Black Pepper.jpg';
+  import Apollo2 from '../../6_Engineered/Black Pepper-.jpg';
+
+</script>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column; /* Stack children vertically */
+    align-items: center;    /* Center children horizontally */
+    max-height: 100vh;      /* Maximum height is the viewport height */
+    overflow-y: auto;       /* Allow vertical scrolling if content overflows */
+    width: 100%;            /* Take full width */
+}
+
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.image-container {
+    display: flex;
+    flex-direction: column; 
+    align-items: center;
+    width: 100%;            /* Take full width */
+}
+
+/* Style for each image */
+.image-container img {
+    max-width: 100%;       /* Images will not exceed the container's width */
+    height: auto;          /* Adjust the height automatically based on the image's aspect ratio */
+    object-fit: cover;     /* Resize the content of the image to fit its container */
+    margin-bottom: 10px;   /* Margin at the bottom of each image for separation */
+}
+</style>
+  
+  <main>
+    <div class="image-container">
+      <img src={Apollo} alt="">
+      <img src={Apollo2} alt="">
+    </div>
+  </main>
